9ea28e888a0b13345d5c6d14f50a1be772de9666
James Jiang <monochrome473@gmail.com>
2020-04-16 00:06:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
 require 'pry'
 def reverse_each_word(sentence)
-  #sentence_array = []
-  reversed_word = ""
-  reversed_sentence = ""
-  sentence_array = sentence.split
+sentence_array = sentence.split
   sentence_array.each do |word|
     reversed_word = word.reverse
     reversed_sentence = reversed_sentence + reversed_word + " "
